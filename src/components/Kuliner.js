3dccdb6b347f04907c38423c6224b5cdbd19c2b1
BantuMasyarakat <khusainimukhamad@gmail.com>
2022-03-04 21:43:46 +0700

    add kuliner

---
+++ src/components/Kuliner.js
@@ -0,0 +1,11 @@
+import css from "../styles/Kuliner.module.css";
+
+const Kuliner = () => {
+    return (
+        <div>
+            <p>Kuliner Boos</p>
+        </div>
+    );
+};
+
+export default Kuliner;
