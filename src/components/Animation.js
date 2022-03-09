f4954f0582d78a32b09a6842f8de49c30601ebd1
BantuMasyarakat <khusainimukhamad@gmail.com>
2022-03-09 17:59:40 +0700

    add kuliner component

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -2,7 +2,7 @@ import css from "../styles/Animation.module.css";
 
 const Animation = () => {
     return (
-        <svg width="100%" height="100%" className={css.svg}>
+        <svg width="10rem" height="100%" className={css.svg}>
             <circle className={css.shape} />
             <circle className={css.shape} />
             <circle className={css.shape} />
--- src/components/Kuliner.js
+++ src/components/Kuliner.js
@@ -1,9 +1,71 @@
 import css from "../styles/Kuliner.module.css";
+import Animation from "./Animation";
 
 const Kuliner = () => {
+    const image = {
+        width: "19rem",
+        height: "10rem",
+        backgroundImage: "url('/img/ft.jpg')",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        borderRadius: "0.5rem",
+    };
+
     return (
-        <div>
-            <p>Kuliner Boos</p>
+        <div className={css.kuliner}>
+            <div className={css.card}>
+                <div style={image} className={css.img}></div>
+                <div className={css.text}>
+                    <h2>Macroon Order Gudeg, Kuliner Gudeg Mendunia</h2>
+                    <h3>M. Kalingga - 20 April 2022</h3>
+                    <hr />
+                    <p>
+                        Sebelumnya Macroon pergi ke Indonesia untuk melakukan kunjungan kenegaraan. Ia mampir ke Jogja untuk mengicipi gudeg
+                        Yu Jum yang mendunia. Ia pun ...
+                    </p>
+                    <a href="/blog/1">Read more &#62;&#62;</a>
+                </div>
+            </div>
+            <div className={css.card}>
+                <div style={image} className={css.img}></div>
+                <div className={css.text}>
+                    <h2>Macroon Order Gudeg, Kuliner Gudeg Mendunia</h2>
+                    <h3>M. Kalingga - 20 April 2022</h3>
+                    <hr />
+                    <p>
+                        Sebelumnya Macroon pergi ke Indonesia untuk melakukan kunjungan kenegaraan. Ia mampir ke Jogja untuk mengicipi gudeg
+                        Yu Jum yang mendunia. Ia pun ...
+                    </p>
+                    <a href="/blog/1">Read more &#62;&#62;</a>
+                </div>
+            </div>
+            <div className={css.card}>
+                <div style={image} className={css.img}></div>
+                <div className={css.text}>
+                    <h2>Macroon Order Gudeg, Kuliner Gudeg Mendunia</h2>
+                    <h3>M. Kalingga - 20 April 2022</h3>
+                    <hr />
+                    <p>
+                        Sebelumnya Macroon pergi ke Indonesia untuk melakukan kunjungan kenegaraan. Ia mampir ke Jogja untuk mengicipi gudeg
+                        Yu Jum yang mendunia. Ia pun ...
+                    </p>
+                    <a href="/blog/1">Read more &#62;&#62;</a>
+                </div>
+            </div>
+            <div className={css.card}>
+                <div style={image} className={css.img}></div>
+                <div className={css.text}>
+                    <h2>Macroon Order Gudeg, Kuliner Gudeg Mendunia</h2>
+                    <h3>M. Kalingga - 20 April 2022</h3>
+                    <hr />
+                    <p>
+                        Sebelumnya Macroon pergi ke Indonesia untuk melakukan kunjungan kenegaraan. Ia mampir ke Jogja untuk mengicipi gudeg
+                        Yu Jum yang mendunia. Ia pun ...
+                    </p>
+                    <a href="/blog/1">Read more &#62;&#62;</a>
+                </div>
+            </div>
         </div>
     );
 };
