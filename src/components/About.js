49344a0b8b9101198fb8a782b15e12911d8abead
BantuMasyarakat <khusainimukhamad@gmail.com>
2022-03-14 21:26:03 +0700

    lazy add details

---
+++ src/components/About.js
@@ -0,0 +1,7 @@
+import css from "../styles/About.module.css";
+
+const About = () => {
+    return <div className={css.about}></div>;
+};
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,7 @@
+import css from "../styles/Contact.module.css";
+
+const Contact = () => {
+    return <div className={css.contact}></div>;
+};
+
+export default Contact;
